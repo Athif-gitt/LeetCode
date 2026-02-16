37487ee80c07887fb95f6c8803086cc522d619a6
Athif Faisal <athiffaisal55@gmail.com>
2026-02-16 12:08:27 +0530

    Time: 0 ms (100%), Space: 21.2 MB (5.64%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,16 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        freq = {}
-        for num in nums:
-            if num in freq:
-                freq[num] +=1
-            else:
-                freq[num] = 1
-
-        for f in freq:
-            if freq[f] == 1:
-                return f
-
+        ideal_list = 3 * sum(set(nums))
+        org_list = sum(nums)
+        diff = ideal_list - org_list
+        return diff // 2
+        
 
             
                     
