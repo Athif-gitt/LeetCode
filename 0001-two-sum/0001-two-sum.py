80d4178e054498c14ebb368f08f5f72bb496576d
Athif Faisal <athiffaisal55@gmail.com>
2026-05-29 15:46:24 +0530

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,6 +4,7 @@ class Solution:
         for i in range(0, len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
+                    
                     indices.append(i)
                     indices.append(j)
         return indices
